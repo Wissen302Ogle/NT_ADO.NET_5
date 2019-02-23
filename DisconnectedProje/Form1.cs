0d7094ff9a443d091a2a7dcd78f5e33a953d1381
Wissen302Ogle <isleyengulcan94@gmail.com>
2019-02-23 16:34:28 +0200

    first commit

---
+++ DisconnectedProje/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DisconnectedProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=WISSEN\MSSQLSRV; Initial Catalog=Northwind; Integrated Security=true");
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT*FROM Products", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT*FROM Products WHERE ProductID=@ID ", baglanti);
+            cmd.Parameters.AddWithValue("@ID", textBox1.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO Products(ProductName,UnitsInStock,UnitPrice) VALUES(@Ad,@Stok,@Fiyat) ", baglanti);
+            cmd.Parameters.AddWithValue("@Ad", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Stok", textBox3.Text);
+            cmd.Parameters.AddWithValue("@Fiyat", textBox4.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
---
+++ UpdateDeleteIslemi/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UpdateDeleteIslemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(Tools.ConnectionString);
+      
+
+        public void KategoriDoldur()
+        {
+            if (baglanti.State == ConnectionState.Closed)
+                baglanti.Open();
+            SqlCommand cmd = new SqlCommand("SELECT CategoryId, CategoryName FROM Categories ", baglanti);
+            SqlDataReader dr = cmd.ExecuteReader();
+           
+            if (dr.HasRows)
+                comboBox1.Items.Clear();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr["CategoryName"].ToString());
+            }
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KategoriDoldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+
+                SqlCommand cmd = new SqlCommand("DELETE FROM Categories WHERE CategoryName=@catName", baglanti);
+                cmd.Parameters.AddWithValue("@catName", comboBox1.SelectedItem.ToString());
+               
+
+
+                int donenDeger = cmd.ExecuteNonQuery();
+                KategoriDoldur();
+                MessageBox.Show(donenDeger != 0 ? "ISLEM BASARILI" : "ISLEM BASARISIZ");
+                baglanti.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+            if (baglanti.State == ConnectionState.Open)
+                baglanti.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+
+                    SqlCommand cmd = new SqlCommand("UPDATE Categories SET CategoryName=@catName WHERE CategoryName=@Name", baglanti);
+                cmd.Parameters.AddWithValue("@catName", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Name", comboBox1.SelectedItem.ToString());
+
+
+                int donenDeger = cmd.ExecuteNonQuery();
+                    KategoriDoldur();
+                    MessageBox.Show(donenDeger != 0 ? "ISLEM BASARILI" : "ISLEM BASARISIZ");
+            baglanti.Close();
+
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+            if (baglanti.State == ConnectionState.Open)
+                baglanti.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    baglanti.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Categories (CategoryName)" +
+                        " VALUES(@catName) ", baglanti);
+
+                    cmd.Parameters.AddWithValue("@catName", textBox1.Text);
+                   
+
+                    int donenDeger = cmd.ExecuteNonQuery();
+                    KategoriDoldur();
+                    MessageBox.Show(donenDeger != 0 ? "ISLEM BASARILI" : "ISLEM BASARISIZ");
+                    baglanti.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+            if (baglanti.State == ConnectionState.Open)
+                baglanti.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
